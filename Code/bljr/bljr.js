7a9d67007d95227afa076130960fa034329333ab
nicolefeliceee <nicolefeliceee@gmail.com>
2022-12-20 19:26:44 +0700

    popup checkout

---
+++ Code/bljr/bljr.js
@@ -0,0 +1,13 @@
+let popUp = document.getElementById("popUp");
+let bg = document.getElementById("bg");
+
+function openPopUp(){
+    popUp.classList.add("open-popUp");
+    bg.classList.add("open-bg");
+}
+
+function closePopUp(){
+    popUp.classList.remove("open-popUp");
+    bg.classList.remove("open-bg");
+}
+
