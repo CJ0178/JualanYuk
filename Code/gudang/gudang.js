ab96757c5161588391e271de3617d16f2ea626ce
nicolefeliceee <nicolefeliceee@gmail.com>
2022-12-23 10:44:29 +0700

    gudang, bikin data brg bisa diedit pake form

--- Code/gudang/gudang.js
+++ Code/gudang/gudang.js
@@ -2,6 +2,12 @@ var edit = document.getElementsByClassName("edit")
 var centang = document.getElementsByClassName("centang");
 var centangKosong = document.getElementsByClassName("centangKosong");
 var sampah = document.getElementsByClassName("sampah");
+var namaAwal = document.getElementsByClassName("namaAwal");
+var nama = document.getElementsByClassName("nama");
+var stokAwal = document.getElementsByClassName("stokAwal");
+var stok2 = document.getElementsByClassName("stok2");
+var hargaAwal = document.getElementsByClassName("hargaAwal");
+var harga2 = document.getElementsByClassName("harga2");
 
 for(let i=0; i<edit.length; i++){
     edit[i].addEventListener('click', function(){
@@ -23,6 +29,40 @@ for(let i=0; i<edit.length; i++){
     centangKosong[i].addEventListener('mouseleave', function(){
         centang[i].classList.toggle("displayNone")
         centangKosong[i].classList.toggle("displayNone")
+    })
+
+    // untuk edit stok -> mati hidupin form
+    edit[i].addEventListener('click', function(){
+        namaAwal[i].classList.toggle("displayNone");
+        nama[i].classList.toggle("displayNone");
+        nama[i].disabled=false;
+    })
+    centang[i].addEventListener('click', function(){
+        namaAwal[i].classList.toggle("displayNone");
+        nama[i].classList.toggle("displayNone");
+        nama[i].disabled=true;
+    })
+
+    edit[i].addEventListener('click', function(){
+        stokAwal[i].classList.toggle("displayNone");
+        stok2[i].classList.toggle("displayNone");
+        stok2[i].disabled=false;
+    })
+    centang[i].addEventListener('click', function(){
+        stokAwal[i].classList.toggle("displayNone");
+        stok2[i].classList.toggle("displayNone");
+        stok2[i].disabled=true;
+    })
+
+    edit[i].addEventListener('click', function(){
+        hargaAwal[i].classList.toggle("displayNone");
+        harga2[i].classList.toggle("displayNone");
+        harga2[i].disabled=false;
+    })
+    centang[i].addEventListener('click', function(){
+        hargaAwal[i].classList.toggle("displayNone");
+        harga2[i].classList.toggle("displayNone");
+        harga2[i].disabled=true;
     })
 }
 
