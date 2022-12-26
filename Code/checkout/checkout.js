b23a2e389499978982591642139eb6b3fbf34a7c
jeffersonnjohan <jeffersonnjohan@gmail.com>
2022-12-26 15:48:11 +0700

    Urus pembayaran pop up

--- Code/checkout/checkout.js
+++ Code/checkout/checkout.js
@@ -5,7 +5,7 @@ buttonCheckout.addEventListener('click', function(){
     document.location.href = '../checkoutProcess.php?list='+parameter
 })
 
-// Untuk pop up
+// Untuk pop up bayar berhasil
 let popUp = document.getElementById("popUp");
 let bg = document.getElementById("bg");
 
@@ -18,3 +18,22 @@ function closePopUp(){
     popUp.classList.remove("open-popUp");
     bg.classList.remove("open-bg");
 }
+
+// Untuk pop up: Jeff
+buttonPilih = document.getElementById('buttonPilih')
+divPopUp = document.getElementsByClassName('popUpBayar')[0]
+svgPanah = document.getElementsByClassName('svgPanah')
+
+buttonPilih.addEventListener('click', function(){
+    divPopUp.classList.remove('displayNone')
+})
+
+listNama = ['DANA', 'OVO', 'GOPAY', 'SHOPEE PAY', 'LINK AJA', 'JENIUS', 'SAKUKU', 'VIRTUAL ACCOUNT']
+
+for(let i = 0; i < svgPanah.length; i++){
+    svgPanah[i].addEventListener('click', function(){
+        buttonPilih.innerHTML = listNama[i]
+        divPopUp.classList.add('displayNone')
+    })
+}
+
