1ce4ddadf2bf713cc3a6d5bb9cb37106dd38096a
nicolefeliceee <nicolefeliceee@gmail.com>
2022-12-27 22:35:43 +0700

    benerin js byw

--- Code/buildYourWarung/buildYourWarung.js
+++ Code/buildYourWarung/buildYourWarung.js
@@ -1,14 +1,14 @@
+var cardPaket = document.getElementsByClassName("cardPaket");
 var cardPaket1 = document.getElementsByClassName("cardPaket1");
 var cardPaket2 = document.getElementsByClassName("cardPaket2");
 
 for(let i=0; i<cardPaket1.length; i++){
-    cardPaket1[i].addEventListener('mouseenter', function(){
-        // console.log("hai")
+    cardPaket[i].addEventListener('mouseenter', function(){
         cardPaket1[i].classList.toggle("displayNone");
         cardPaket2[i].classList.toggle("displayNone");
     })
-    
-    cardPaket1[i].addEventListener('mouseleave', function(){
+
+    cardPaket[i].addEventListener('mouseleave', function(){
         cardPaket1[i].classList.toggle("displayNone");
         cardPaket2[i].classList.toggle("displayNone");
     })
