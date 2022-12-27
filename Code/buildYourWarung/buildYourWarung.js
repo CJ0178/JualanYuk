ef17ef367863866f2a867ba55ec9220845941dc4
nicolefeliceee <nicolefeliceee@gmail.com>
2022-12-28 00:19:51 +0700

    byw selesai

--- Code/buildYourWarung/buildYourWarung.js
+++ Code/buildYourWarung/buildYourWarung.js
@@ -13,3 +13,30 @@ for(let i=0; i<cardPaket1.length; i++){
         cardPaket2[i].classList.toggle("displayNone");
     })
 }
+
+// Plus min
+initPlusMin()
+
+function initPlusMin(){
+    buttonMinus = document.getElementsByClassName('kotakMin')
+    buttonPlus = document.getElementsByClassName('kotakTambah')
+    inputQty = document.getElementsByClassName('qtyBeli')
+    
+    for(let i = 0; i < buttonMinus.length; i++){
+        buttonPlus[i].addEventListener('click', ()=>{
+            inputQty[i].value++
+            // Submit form yang bersangkutan
+            document.getElementsByClassName('qty'+i)[0].submit()
+        })
+        
+        buttonMinus[i].addEventListener('click', ()=>{
+            if(inputQty[i].value-1 >= 1){
+                inputQty[i].value--
+                document.getElementsByClassName('qty'+i)[0].submit()
+            } else{
+                inputQty[i].value = 1
+                document.getElementsByClassName('qty'+i)[0].submit()
+            }
+        })   
+    }
+}
